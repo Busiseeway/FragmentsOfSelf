51e34c07936dbc425dd9b091804759aee953e210
Lemogang Matlou <lemogangmatlou311@gmail.com>
2025-10-23 20:30:30 +0200

    Revert "Level1modular"

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
 const path = window.location.pathname;
 
-if (path === "/level1") {
-  import("./Level1/main1.js")
-    .then((level1) => {
-      level1.startLevel1();
-    })
-    .catch((err) => console.error("Failed to load level:", err));
-} else {
-  console.error("Level not found");
 if (path === '/level3') {
     import('./Level3/main3.js').then(level3 => {
         level3.startLevel3();
