1841daad4a02f2d762402871cc85fcda44dfff87
mukondi29 <808713@students.wits.ac.za>
2025-10-24 11:06:56 +0200

    Merging level1 to main

--- main.js
+++ main.js
@@ -4,6 +4,14 @@ if (path === '/level3') {
     import('./Level3/main3.js').then(level3 => {
         level3.startLevel3();
     }).catch(err => console.error("Failed to load level:", err));
+} 
+
+else if (path === "/level1") {
+  import("./Level1/main1.js")
+    .then((level1) => {
+      level1.startLevel1();
+    })
+    .catch((err) => console.error("Failed to load level:", err));
 } else {
-    console.error("Level not found");
+  console.error("Level not found");
 }
